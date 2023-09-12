49bdff56fd7df72d528d08d4250d743fdec2ebc3
Osamuyi <muyixone@yahoo.com>
2023-09-12 04:21:27 +0100

    Create person services

---
+++ src/services/personServices.js
@@ -0,0 +1,19 @@
+const CreatePerson = () => {
+  return;
+};
+const getPerson = () => {
+  return;
+};
+const updateOnePersonInfo = () => {
+  return;
+};
+const deleteOnePerson = () => {
+  return;
+};
+
+module.exports = {
+  CreatePerson,
+  getPerson,
+  updateOnePersonInfo,
+  deleteOnePerson,
+};
