4223ea7344b942d4cc9fb367fab8f563661f1b07
Osamuyi <muyixone@yahoo.com>
2023-09-12 06:37:56 +0100

    Connect to Db

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const app = require('./src/app');
+const connectDb = require('./Db/db');
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log('App is running');
-});
+const start = async () => {
+  try {
+    await connectDb(process.env.MONGODB_URI);
+    app.listen(PORT, () => {
+      console.log('App is running');
+    });
+  } catch (error) {}
+};
+
+start();
--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const persons = require('./routes/personRoute');
+const personRoute = require('./routes/personRoute');
 require('dotenv').config();
 
 const app = express();
 
-app.use('/hng/api/v1', persons);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/hng/api/v1', personRoute);
+
+app.use('*', (req, res) => {
+  res.send('Page not found');
+});
 
 module.exports = app;
