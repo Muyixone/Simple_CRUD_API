8558932a80bd973e75c34790dbb68bdcba8ae935
Osamuyi <muyixone@yahoo.com>
2023-09-13 05:21:07 +0100

    Create updateInfo function

--- src/controllers/personController.js
+++ src/controllers/personController.js
@@ -1,22 +1,59 @@
-const CreatePerson = (req, res) => {
-  res.json({
-    mesg: 'person created',
-  });
+const mongoose = require('mongoose');
+const personService = require('../services/personServices');
+
+const CreatePerson = async (req, res) => {
+  try {
+    const person = await personService.CreatePerson({ ...req.body });
+
+    return res.json({
+      mesg: 'person created',
+      person,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const getPerson = (req, res) => {
-  res.json({
-    mesg: 'Get single person',
-  });
+/* 
+    @desc Get single person
+    @route GET /api/v1/:user_id
+    @access Public
+*/
+const getPerson = async (req, res) => {
+  const { user_id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(user_id)) {
+    return console.log(`No item with ${user_id} found`);
+    // throw new notFoundError(`No item found with id ${stateId}`);
+  }
+
+  try {
+    const person = await personService.getPerson(user_id);
+    res.json({
+      person,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const updateOnePersonInfo = (req, res) => {
+const updateOnePersonInfo = async (req, res) => {
+  const { user_id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(user_id)) {
+    return console.log(`No item with ${user_id} found`);
+    // throw new notFoundError(`No item found with id ${stateId}`);
+  }
+  const person = await personService.updateOnePersonInfo(user_id, req.body);
+
   res.json({
     mesg: 'person  info updated',
+    person,
   });
 };
 
 const deleteOnePerson = (req, res) => {
+  const person = personService.deleteOnePerson();
   res.json({
     mesg: 'person deleted',
   });
--- src/services/personServices.js
+++ src/services/personServices.js
@@ -1,11 +1,44 @@
-const CreatePerson = () => {
-  return;
+const personModel = require('../models/personModel');
+
+const CreatePerson = async (personData) => {
+  const person = new personModel({ ...personData });
+
+  try {
+    await person.save();
+    return person;
+  } catch (error) {
+    console.log(error);
+  }
 };
-const getPerson = () => {
-  return;
+
+const getPerson = async (personId) => {
+  try {
+    const person = await personModel.findById({ _id: personId });
+
+    if (!person) {
+      return console.log('Person not found');
+    }
+
+    return person;
+  } catch (error) {
+    console.log(error);
+  }
 };
-const updateOnePersonInfo = () => {
-  return;
+const updateOnePersonInfo = async (personId, newInfo) => {
+  try {
+    const person = await personModel.findById({ _id: personId });
+    if (!person) {
+      console.log('Person not found');
+      return null;
+    }
+
+    person.set(newInfo);
+
+    await person.save();
+    return person;
+  } catch (error) {
+    console.log(error);
+  }
 };
 const deleteOnePerson = () => {
   return;
