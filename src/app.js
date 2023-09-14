ff9b2096991214668a7055aa3d99cf8190a14c51
Osamuyi <muyixone@yahoo.com>
2023-09-14 09:29:23 +0100

    Increase Ping to 15mins

--- src/app.js
+++ src/app.js
@@ -17,13 +17,13 @@ app.use('*', (req, res) => {
   res.send('Page not found');
 });
 
-// Periodically ping the self-ping endpoint (every 5 minutes)
+// Periodically ping the self-ping endpoint (every 15 minutes)
 setInterval(() => {
   // Make an HTTP request to the /ping endpoint of your own API
   const http = require('http');
   http.get('https://crud-api-i52y.onrender.com/hng/api/ping', (res) => {
     console.log(`Ping response: ${res.statusCode}`);
   });
-}, 5 * 60 * 1000); // Every 5 minutes
+}, 15 * 60 * 1000); // Every 5 minutes
 
 module.exports = app;
